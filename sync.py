1c0a0929a0027b471af13af3a10fe1d5f1038710
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-08-20 13:55:51 +0200

    lower time targets than time now will now execute on the next day

--- sync.py
+++ sync.py
@@ -7,10 +7,14 @@ import os
 os.chdir("./Media")
 
 args = sys.argv
-m = str(args[1])
-time_to_play = None
-if len(args) > 2:
-    time_to_play = str(args[2])
+try:
+    m = str(args[1])
+    time_to_play = None
+    if len(args) > 2:
+        time_to_play = str(args[2])
+except:
+    print("Wrong arguments! Should be:\npython3 sync.py <filename> <time>[format: HH:MM:SS]")
+    exit(1)
 
 # Setting up Media
 player = vlc.MediaPlayer()
@@ -29,9 +33,14 @@ if (time_to_play != None):
     target_H = int(time_to_play[0:2])
     target_M = int(time_to_play[3:5])
     target_S = int(time_to_play[6:8])
-    seconds_to_wait = (target_H*60*60 + target_M*60 + target_S) - (now_H*60*60 + now_M*60 + now_S )
+    now_in_seconds = (now_H*60*60 + now_M*60 + now_S )
+    target_in_seconds = (target_H*60*60 + target_M*60 + target_S) 
+    if target_in_seconds - now_in_seconds >= 0:
+        seconds_to_wait = target_in_seconds - now_in_seconds
+    else:
+        seconds_to_wait = 24*60*60 - now_in_seconds + target_in_seconds
 
-    print(time.strftime("Time now: %H:%M:%S", now))
+    print(time.strftime("Time at excecution: %H:%M:%S", now))
     print("Seconds to wait:")
 
     while(seconds_to_wait > 0):
@@ -43,7 +52,6 @@ if (time_to_play != None):
 player.play()
 time.sleep(0.05)
 duration = player.get_length()
-print(duration)
 player.play()
 time.sleep(duration)
 # time.sleep(media.get_duration())
